114bf874b7e4d9d1f5b9b56c63ec00588508dbae
tgliati <thanosgliatis@gmail.com>
2016-04-19 15:12:56 -0500

    this a change in clone

--- MyFirstJavaProject/src/com/example/main/Hello.java
+++ MyFirstJavaProject/src/com/example/main/Hello.java
@@ -3,6 +3,6 @@ package com.example.main;
 public class Hello {
 	
 	public static void main(String[] args){
-		System.out.println("Integrate java project with Git..Commit.");
+		System.out.println("Integrate java project with Git..Commit...clone.");
 	}
 	}
